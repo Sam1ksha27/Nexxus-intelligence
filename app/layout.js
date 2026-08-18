1587c92e43c3a8049fe5815ade6c353ad9551d2c
Sam1ksha27 <samikshatupe0@gmail.com>
2026-08-18 18:58:40 +0530

    Initial Nexxus Intelligence prototype

---
+++ app/layout.js
@@ -0,0 +1,3 @@
+import './globals.css';
+export const metadata={title:'Nexxus Intelligence',description:'Science that learns your hair.'};
+export default function RootLayout({children}){return <html lang="en"><body>{children}</body></html>}
