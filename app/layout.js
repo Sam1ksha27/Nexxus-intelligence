a48e9fcf2d5d74cad0e0e1a2a7aeebceed7a7fb7
Sam1ksha27 <samikshatupe0@gmail.com>
2026-08-20 18:53:41 +0530

    Activate S account menu for My Nexxus

--- app/layout.js
+++ app/layout.js
@@ -4,5 +4,6 @@ import './logo.css';
 import './personalization.css';
 import './how.css';
 import GlobalActions from './GlobalActions';
+import RouteBridge from './RouteBridge';
 export const metadata={title:'Nexxus Intelligence',description:'Science that learns your hair.'};
-export default function RootLayout({children}){return <html lang="en"><body>{children}<GlobalActions/></body></html>}
+export default function RootLayout({children}){return <html lang="en"><body>{children}<GlobalActions/><RouteBridge/></body></html>}
