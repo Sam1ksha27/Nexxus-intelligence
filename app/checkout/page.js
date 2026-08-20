3917dbfad992756f002cb527450517889e34b4ef
Sam1ksha27 <samikshatupe0@gmail.com>
2026-08-20 18:15:22 +0530

    Keep checkout on Nexxus root URL

--- app/checkout/page.js
+++ app/checkout/page.js
@@ -1,5 +1,7 @@
 'use client';
+import { useEffect } from 'react';
 import Buy from '../buy/page';
 export default function CheckoutPage(){
+  useEffect(()=>{ window.history.replaceState({},'', '/'); },[]);
   return <Buy />;
 }
