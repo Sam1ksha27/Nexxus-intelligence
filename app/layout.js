44cdcf7ae9387839528c4fc1fc72748c575fb92a
Sam1ksha27 <samikshatupe0@gmail.com>
2026-08-21 11:22:07 +0530

    Load responsive dashboard styles

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,7 @@ import './review-restore.css';
 import './logo.css';
 import './personalization.css';
 import './how.css';
+import './dashboard-responsive.css';
 import GlobalActions from './GlobalActions';
 import RouteBridge from './RouteBridge';
 export const metadata={title:'Nexxus Intelligence',description:'Science that learns your hair.'};
